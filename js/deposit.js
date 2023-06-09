9e4116a43d82747886334b5516c9cd053364b676
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-06-10 02:15:20 +0600

    create withdraw & reduce balance field

--- js/deposit.js
+++ js/deposit.js
@@ -3,11 +3,28 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
    // console.log('hello')
    //get the deposit amount from the deposit input field
    const depositField= document.getElementById('deposit-field');
-   const depositAmount= depositField.value;
+   const newDepositAmountString= depositField.value;
+   const newDepositAmount=parseFloat(newDepositAmountString);
   // console.log(depositAmount)
 
    const depositTotalElement= document.getElementById('deposit-total');
-   const depositTotal= depositTotalElement.innerText;
-   depositTotalElement.innerText=depositAmount;
+   const previousDepositTotalString= depositTotalElement.innerText;
+   const previousDepositTotal=parseFloat(previousDepositTotalString);
+   
+   const currentDepositTotal= previousDepositTotal + newDepositAmount;
+   depositTotalElement.innerText=currentDepositTotal;
+
+
+   // get current balance
+
+   const balanceTotalElement= document.getElementById('balance-total');
+   const previousBalanceTotalString= balanceTotalElement.innerText;
+   const previousBalanceTotal= parseFloat(previousBalanceTotalString);
+
+   const currentBalanceTotal= previousBalanceTotal + newDepositAmount;
+   balanceTotalElement.innerText=currentBalanceTotal;
+
+
+   depositField.value='';
 
 })
