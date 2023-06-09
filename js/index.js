6a6d997e6195fcf6654aafa28cc84770fbd490a2
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-06-10 00:16:50 +0600

    navigate one to another page, create deposit, withdraw & balance card

--- js/index.js
+++ js/index.js
@@ -6,9 +6,16 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     //get the email address inside the email input field
     const emailFiled= document.getElementById('user-email');
     const email= emailFiled.value;
-    console.log(email)
+    //console.log(email)
 
    const passwordField=document.getElementById('user-password')
    const password= passwordField.value;
-   console.log(password)
+  // console.log(password)
+  if(email==='hello@gmail.com' && password==='123456'){
+    // console.log('yes')
+    window.location.href='bank.html';
+  }
+  else{
+    alert('invaild user');
+  }
 })
